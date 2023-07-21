6ab1e417ef94d65723648c0e889594b3e6765e99
xWatchR <sebastianmissler@web.de>
2023-07-21 19:31:49 +0200

    navbar

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -4,7 +4,7 @@ import NavItems from './NavItems.vue'
 
 <template>
   <v-app-bar color="grey-darken-4" height="80">
-    <div class="nav-title" @click="$router.push('/')">Kinkify</div>
+    <router-link to="/" class="nav-title">Kinkify</router-link>
     <v-spacer></v-spacer>
     <NavItems />
   </v-app-bar>
@@ -16,6 +16,7 @@ import NavItems from './NavItems.vue'
   font-size: 1.3rem;
   transition: transform 0.9s ease-out;
   padding: 2rem;
+  color: white;
 }
 .nav-title:hover {
   cursor: pointer;
